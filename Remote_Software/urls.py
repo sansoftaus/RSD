0181a742a6b60f7fc227c415260e1b972629d05b
root <root@ubuntu.ubuntu-domain>
2013-06-20 14:44:21 +0100

    Third Commit

--- Remote_Software/urls.py
+++ Remote_Software/urls.py
@@ -1,10 +1,13 @@
 from django.conf.urls import patterns, include, url
+from Remote_Software.views import hello
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
 
 urlpatterns = patterns('',
+    
+    url(r'^hello/$', hello),
     # Examples:
     # url(r'^$', 'Remote_Software.views.home', name='home'),
     # url(r'^Remote_Software/', include('Remote_Software.foo.urls')),
